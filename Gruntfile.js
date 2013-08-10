c31b0543316a475a9ae7696dff109e4b873c1ece
Christian Bradley <christianbradley@gmail.com>
2013-08-10 04:26:10 -0500

    Added grunt task for linting JSON

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,22 @@
+"use strict";
+
 module.exports = function(grunt) {
-  "use strict";
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    jshint: require("./config/grunt/jshint/config.js")
+    jshint: require("./config/grunt/jshint/config.js"),
+    exec: {
+      jsonlint: {
+        command: "scripts/jsonlint-all.sh"
+      }
+    }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-exec');
+
+  grunt.registerTask('jsonlint', ['exec:jsonlint']);
+  grunt.registerTask('build', ['jshint', 'jsonlint']);
+  grunt.registerTask('default', ['build']);
 };
 
--- config/grunt/jshint/config.js
+++ config/grunt/jshint/config.js
@@ -3,13 +3,6 @@
 module.exports = {
 
   options: require("./default-options.js"),
-
-  // Use JSON lint?
-  json: {
-    options: { strict: false },
-    files: { src: ["**/*.json"] },
-  },
-
   scripts: require("./scripts.js"),
   specs: require("./specs.js"),
   browser: require("./browser.js")
